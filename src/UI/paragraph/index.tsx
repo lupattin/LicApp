6cba12e245e15a080bed13a31b8fc32a3c6513ba
unknown <ramiromaslat@gmail.com>
2023-03-13 19:04:36 -0300

    fix merge 2

--- src/UI/paragraph/index.tsx
+++ src/UI/paragraph/index.tsx
@@ -13,13 +13,8 @@ export const ParagraphHomePage = styled(Paragraph)`
 `;
 
 export const ParagraphAboutMe = styled(Paragraph)`
-<<<<<<< HEAD
   font-size: 1vw;
   font-weight: 100;
-=======
-  font-size: 16px;
-  font-weight: 700;
->>>>>>> e75f040465ca55c6f2ad7708d0f01c596b5dd7b9
   color: white;
   width: 100%;
   text-align: start;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,7 @@ import { Lexend } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const lexend = Lexend({
-<<<<<<< HEAD
   weight: ['100','200', '300', '400','600','700'],
-=======
-  weight: ['100','400','700'],
->>>>>>> e75f040465ca55c6f2ad7708d0f01c596b5dd7b9
   style:['normal'],
   subsets: ['latin'],
 })
