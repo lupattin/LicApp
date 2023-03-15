bbd714c201c2b175cb02477bd399d7b2035b77c6
unknown <ramiromaslat@gmail.com>
2023-03-15 18:49:02 -0300

    add footer and styles

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,19 @@
+/*UI*/
+import { FooterContainer } from "@/UI/containers"
+import { TitleFooter } from "@/UI/title"
+import { FooterImgLogo } from "@/UI/img"
+import { FooterImgContainer } from "@/UI/containers"
+import { FooterList } from "@/UI/list" 
+
+
+export function FooterBox () {
+    return (
+        <FooterContainer>          
+            <FooterList className={'listado'}></FooterList>
+            <FooterImgContainer>
+            <FooterImgLogo className='footerImgLogo' ImgUrl='https://nutriloca.com/wp-content/uploads/2021/05/aboutme-10.png'></FooterImgLogo>
+            </FooterImgContainer>
+            <div style={{color: "white", margin:"auto"}}>aca van los logos de redes sociales</div>
+        </FooterContainer>
+    )
+}
