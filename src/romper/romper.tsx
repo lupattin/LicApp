910902bd450f99c58b6e8c812cdbce4ab22b6bb6
Luca <lupattin@gmail.comgit config --global user.email lupattin@gmail.comgit config --global user.email lupattin@gmail.com>
2023-03-17 17:43:53 -0300

    testing git

---
+++ src/romper/romper.tsx
@@ -0,0 +1,21 @@
+
+export function Luca() {
+  
+    return (
+    
+        <div>Romper Luca</div>
+      
+    );
+  }
+
+
+
+
+export function Rama() {
+  
+    return (
+    
+        <div>Romper Rama</div>
+      
+    );
+  }
