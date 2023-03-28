ecc804a97966114dd9310a60d10aee86747b49b0
Luca <lupattin@gmail.com>
2023-03-28 14:57:43 -0300

    Changes media

--- src/components/homebox/index.tsx
+++ src/components/homebox/index.tsx
@@ -27,13 +27,14 @@ export function HomeBox() {
     <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
       <LogoImg className={"animate__animated animate__fadeIn animate__delay-1s"} ImgUrl={LogoPic} Width={350} Height={200} ></LogoImg>
       <TitleHomePage className={"animate__animated animate__fadeInDown animate__delay-2s "}  title={"Lic. Agustina Scasso."} />
+       <p className="animate__animated animate__fadeInDown animate__delay-2s ">Psicóloga</p>
       <HomeParagraphCointainer>
         <ParagraphHomePage onClick={handleShow} className={"animate__animated animate__fadeInLeft animate__delay-3s"} text={'COMENZÁ TU TRATAMIENTO'} />
       </HomeParagraphCointainer>
       <HomeLogoContainer>
         <IgPic className="animate__animated animate__fadeInDown animate__delay-4s" ImgUrl={instagramPic} Page={"https://www.instagram.com/"}></IgPic>
-        <LinkedinPic className="animate__animated animate__fadeInDown animate__delay-4s" ImgUrl={linkdinPic} Page={"https://www.linkedin.com/"}></LinkedinPic>
-        <WppPic className="animate__animated animate__fadeInDown animate__delay-4s" ImgUrl={whatsappPic} Page={"https://web.whatsapp.com/"}></WppPic>
+        <LinkedinPic className="animate__animated animate__fadeInDown animate__delay-4s" ImgUrl={linkdinPic} Page={"https://ar.linkedin.com/in/agustina-scasso-a69a6b142"}></LinkedinPic>
+        <WppPic className="animate__animated animate__fadeInDown animate__delay-4s" ImgUrl={whatsappPic} Page={"https://api.whatsapp.com/send?phone=5492944149524"}></WppPic>
       </HomeLogoContainer>
 
       <ModalSendEmail show={show} handleClose={handleClose}></ModalSendEmail>
